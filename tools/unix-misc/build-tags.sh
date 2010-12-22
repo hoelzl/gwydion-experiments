becdbffed398290513ba627a5c4e5e52c44ba88f
Matthias Hölzl <tc@xantira.com>
2010-12-22 03:22:43 +0100

    Add a shell script to generate tags, ignore TAGS file.

---
+++ tools/unix-misc/build-tags.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -f TAGS
+find d2c/compiler d2c/runtime -name *.dylan | xargs -n1 etags -a -l none -r "/define \(\(sealed\|open\|abstract\|concrete\|primary\|free\|inline\|movable\|flushable\|functional\|\/\*\s*exported\s*\*\/\) \)*\(method\|generic\|function\|class\|variable\|constant\) \([-A-Za-z0-9!&*<>|^$%@_?=]+\)/" -r "/[ \t]*\(\(virtual\|constant\|sealed\|instance\|class\|each-subclass\|\/\*\s*exported\s*\*\/\) \)*slot \([-A-Za-z0-9!&*<>|^$%@_?=]+\)/" -
+
+
